d9da0e5e3216a82ab793ebb8a28fb8ab6de82d0f
plotzky <dev.plotzky@gmail.com>
2024-05-26 14:09:28 -0300

    Fix bash.sh path

--- bash.sh
+++ bash.sh
@@ -9,11 +9,15 @@
 # }
 #
 
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Verifica se a função já está  no .bashrc
 if ! grep -qF "bible() {" ~/.bashrc; then
+
+    script="$script_dir/main.py"
+
     # Adiciona função bible ao final do .bashrc
-    echo -e '\nbible() {\n  script="/home/plotzky/PycharmProjects/Python/pythonProject/run_terminal.py"\n  python "$script" "$@"\n}' >> ~/.bashrc
+    echo -e "\nbible() {\n  python \"$script\"\n}" >> ~/.bashrc
 
     # Atualize o .bashrc
     source ~/.bashrc
